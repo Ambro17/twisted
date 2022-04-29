83c125ee8ea17d2e4654f7bdca944d1a886fc927
Nahuel Ambrosini <ambro17.1@gmail.com>
2022-04-28 21:52:44 -0600

    🔧 Add env var required for socket mode

--- twisted/slack_api.py
+++ twisted/slack_api.py
@@ -1,15 +1,19 @@
-from slack_bolt import App
 import os
+
+from loguru import logger
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
 
 slack_app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
     signing_secret=os.environ["SLACK_SIGNING_SECRET"]
 )
 
-
 @slack_app.event("reaction_added")
-def handle_app_mentions(body, say, logger):
-    logger.info(body)
-    say("What's up?")
+def handle_app_mentions(body):
+    logger.debug(body)
+
+SocketModeHandler(slack_app, os.environ["SLACK_APP_TOKEN"]).start()
 
 
