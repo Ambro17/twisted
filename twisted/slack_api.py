87968c0df9dd91f27d2f78aaaa61ee03a216304a
Nahuel Ambrosini <ambro17.1@gmail.com>
2022-05-16 22:40:08 -0300

    ✨ Send message on special reactions

--- twisted/slack_api.py
+++ twisted/slack_api.py
@@ -7,5 +7,11 @@ slack_app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
 
 
 @slack_app.event("reaction_added")
-def handle_app_mentions(body):
+def handle_app_mentions(event, body, say):
+    """When a reaction is added, do something"""
     logger.debug(body)
+    reaction = event['reaction']
+    if reaction == 'github':
+        say('Hey, you reacted with github emoji :github:')
+    elif reaction == 'twist2':
+        say('Hey, you reacted with twist emoji :twist2:')
