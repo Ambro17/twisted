622dbf4fd68a38f5fd3149ce7994c84313370ca0
Nahuel Ambrosini <ambro17.1@gmail.com>
2022-09-18 22:21:29 -0300

    ✨ Reference bucket and table that store tfstate and state lock respectively. They are created outside terraform since they should never be deleted, they are a precondition of terraform

--- deployment/infra/1.providers.tf
+++ deployment/infra/1.providers.tf
@@ -6,6 +6,12 @@ terraform {
     }
   }
   required_version = ">= 0.14.9"
+  backend "s3" {
+    bucket = "twisted-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-1"     
+    dynamodb_table = "terraform-state" # Store a lock on each terraform write operation to avoid race conditions
+  }
 }
 
 provider "aws" {
